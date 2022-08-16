7111e8062c07e164bb4b6787f13a48bf3dc214b2
Wilbur <b10923010@gemail.gemail.yuntech.edu.tw>
2022-08-16 23:37:56 +0800

    first create

---
+++ android/app/src/main/kotlin/com/example/frontpage/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.frontpage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'homePage.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePageWidget(),
+    );
+  }
+}
